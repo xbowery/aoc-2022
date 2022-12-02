1548a63dc97f4db3a5df4e03a7bf926987894134
xbowery <69230356+xbowery@users.noreply.github.com>
2022-12-02 16:29:44 +0800

    Create automatic way to generate input text file

---
+++ generate_input.py
@@ -0,0 +1,38 @@
+import os
+import requests
+
+from dotenv import load_dotenv
+
+load_dotenv("./.env")
+COOKIE = os.getenv("cookie")
+
+
+def main():
+    cookies = {'session': COOKIE}
+    day = get_day()
+    url = f"https://adventofcode.com/2022/day/{day}/input"
+
+    text = requests.get(url, cookies=cookies).text
+
+    f = open("input.txt", "w")
+    f.write(text)
+
+
+def get_day():
+    valid = False
+    while not valid:
+        try:
+            day = int(input("Which day would you like to solve (1 to 24)? "))
+        except TypeError:
+            print("Sorry, please only enter an integer from 1 to 24.")
+
+        if (day >= 1 and day <= 24):
+            valid = True
+        else:
+            print("Sorry, please only enter an integer from 1 to 24.")
+    return day
+
+
+
+if __name__ == "__main__":
+    main()
